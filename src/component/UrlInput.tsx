033498d58d4abb3fca00c787f571efce4313b5da
rupam71 <rupamsahriar@gmail.com>
2023-05-15 02:51:41 +0600

    url get

--- src/component/UrlInput.tsx
+++ src/component/UrlInput.tsx
@@ -1,23 +1,38 @@
 import { TextField } from "@mui/material";
+import axios from "axios";
 import React, { useState } from "react";
 
 const UrlInput = () => {
-  const [urlLink, seturlLink] = useState("");
+  const [longUrl, setlongUrl] = useState("");
+  const [result, setresult] = useState("")
+
   const handleOnKeyDown = () => {
-    console.log({ urlLink });
+    console.log({ longUrl });
+    axios.post(`${process.env.NEXT_PUBLIC_URL}/api/tinyUrl`,{
+      longUrl
+    })
+    .then(res=>{
+      if(res.data.statusCode===200) setresult(res.data.result)
+    })
   };
   return (
     <div className="url-input">
       <TextField
-        onChange={(event) => seturlLink(event.target.value)}
+        onChange={(event) => setlongUrl(event.target.value)}
         onKeyDown={(event) => event.key === "Enter" && handleOnKeyDown()}
-        value={urlLink}
-        sx={{ width: "800px" }}
+        value={longUrl}
+        sx={{ width:'100%',maxWidth:'600px' }}
         id="filled-basic"
         label="Input Url"
         placeholder="Place Your Big Url"
         variant="filled"
       />
+      {
+        result && <div>
+          Your Short Code Is {`${process.env.NEXT_PUBLIC_URL}/${result}`}
+          <button onClick={()=>navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_URL}/${result}`)}>copy to clipboard</button>
+        </div>
+      }
     </div>
   );
 };
